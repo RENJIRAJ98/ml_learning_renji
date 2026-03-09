8fb57e65c1a41408af373a85cbf94e80a95246f5
RENJI RAJ <renjirajnielit@gmail.com>
2026-03-09 17:29:55 +0530

    Add matrix multiplication example using NumPy

---
+++ 01_numpy_basics/matrix_multiplication.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+# Create matrices
+A = np.array([[1, 2],
+              [3, 4]])
+
+B = np.array([[5, 6],
+              [7, 8]])
+
+# Matrix multiplication
+result = np.dot(A, B)
+
+print("Matrix A:")
+print(A)
+
+print("\nMatrix B:")
+print(B)
+
+print("\nMatrix Multiplication Result:")
+print(result)
