47e1f0be48c0eb843192c0a75fc3db4d83f0bbdf
RENJI RAJ <renjirajnielit@gmail.com>
2026-03-09 17:21:17 +0530

    Update train_test_split.py

--- 06_model_training/train_test_split.py
+++ 06_model_training/train_test_split.py
@@ -1,1 +1,14 @@
+from sklearn.model_selection import train_test_split
+import numpy as np
 
+# Example dataset
+X = np.arange(100).reshape((50,2))
+y = np.arange(50)
+
+# Split dataset
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+print("Training samples:", len(X_train))
+print("Testing samples:", len(X_test))
