2939a2f7c0ad54fe245c1054e9f5afc348a67db4
RENJI RAJ <renjirajnielit@gmail.com>
2026-03-09 17:24:06 +0530

    Add example for creating 1D and 2D NumPy arrays

---
+++ 01_numpy_basics/2arrays_example.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# Create 1D array
+a = np.array([1, 2, 3, 4, 5])
+print("1D Array:", a)
+
+# Create 2D array
+b = np.array([[1, 2, 3],
+              [4, 5, 6]])
+
+print("\n2D Array:")
+print(b)
+
+# Check shape
+print("\nShape:", b.shape)
