34f907f82677c084e81b5fcc59c3a0732e1146f6
RENJI RAJ <renjirajnielit@gmail.com>
2026-03-09 17:27:55 +0530

    Add statistics example using NumPy

---
+++ 01_numpy_basics/statistics_example.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+data = np.array([10, 20, 30, 40, 50])
+
+print("Mean:", np.mean(data))
+print("Median:", np.median(data))
+print("Standard Deviation:", np.std(data))
+print("Minimum:", np.min(data))
+print("Maximum:", np.max(data))
