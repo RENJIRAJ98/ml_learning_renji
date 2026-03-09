f3803ef415b710fa989f5aec7fd5b23659a3dbb3
RENJI RAJ <renjirajnielit@gmail.com>
2026-03-09 17:28:51 +0530

    Add random array example using NumPy

---
+++ 01_numpy_basics/random_array_example.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Random values between 0 and 1
+random_data = np.random.rand(3,3)
+
+print("Random Matrix:")
+print(random_data)
+
+# Random integers
+random_int = np.random.randint(1, 10, size=(2,4))
+
+print("\nRandom Integers:")
+print(random_int)
